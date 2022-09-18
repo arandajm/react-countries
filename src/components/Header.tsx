6fbb3c37ee55c3019915b8e7511e272dfe7d0308
Jesus Aranda <jesus.aranda@gruposantander.com>
2022-09-18 15:18:50 +0200

    adding global styles to app using styled-components

---
+++ src/components/Header.tsx
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const HeaderStyled = styled.header`
+  color: white;
+  height: 80px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  div.container {
+    max-width: 1280px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+  }
+
+  h1 {
+    font-size: 18px;
+  }
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: white;
+`;
+
+const Header = () => {
+  return (
+    <HeaderStyled>
+      <div className="container">
+        <StyledLink to="/">
+          <h1>Where in the world?</h1>
+        </StyledLink>
+        <span>TODO mode switch</span>
+      </div>
+    </HeaderStyled>
+  );
+};
+
+export default Header;
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+
+import GlobalStyle from "./global.css";
+import "./font.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
+      <GlobalStyle />
       <App />
     </BrowserRouter>
   </React.StrictMode>
